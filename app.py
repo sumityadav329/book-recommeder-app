8947af530e7cb5538c8e6ea5899f7e8fbc1993c9
Sumit Yadav <sumityadav329@gmail.com>
2024-07-01 17:58:59 +0530

    csv data loaded  and save as DataFrame

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from src.logger  import logging
-from src.data_loader import download_data
+from src.data_loader import download_data, load_csv
 from src.exception import CustomException
 
 def main():
@@ -8,6 +8,9 @@ def main():
         data_url = 'https://gist.githubusercontent.com/jaidevd/23aef12e9bf56c618c41/raw/c05e98672b8d52fa0cb94aad80f75eb78342e5d4/books_new.csv'
         data_save_path = 'artifacts/books.csv'
         download_data(data_url, data_save_path)
+
+        # Loading the CSV file
+        df = load_csv(data_save_path)
 
     except CustomException as e:
         logging.error(f"An error occurred: {e}")
--- src/data_loader.py
+++ src/data_loader.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import sys
+import pandas as pd
 from src.exception import CustomException
 from src.logger  import logging
 
@@ -15,3 +16,12 @@ def download_data(url: str, save_path: str):
         logging.error(f"Error downloading data: {e}")
         raise CustomException(e, sys)
 
+def load_csv(file_path: str) -> pd.DataFrame:
+    try:
+        logging.info(f"Loading CSV file from {file_path}")
+        df = pd.read_csv(file_path)
+        logging.info(f"CSV file loaded successfully")
+        return df
+    except Exception as e:
+        logging.error(f"Error loading CSV file: {e}")
+        raise CustomException(e, sys)
